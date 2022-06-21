f516e910c2219620c910f55b251af6bc92300890
snakesch <shechunhing@yahoo.com.hk>
2022-06-21 17:45:06 +0800

    genome masking and helper functions

--- src/buildMaskedGenome.sh
+++ src/buildMaskedGenome.sh
@@ -6,41 +6,36 @@ REGION_BED=$3
 PRIORITY_COMPONENT=$4
 OUTPATH=$5
 
-REF_GENOME="/home/louisshe/shortVariantVCF/ref/ucsc.hg19.fasta"
-TOTAL_BED="/home/louisshe/shortVariantVCF/ref/ucsc.hg19.bed"
-REGION_BED="/home/louisshe/shortVariantVCF/ref/homologous_regions/TERT_related_homo_regions.bed"
-PRIORITY_COMPONENT="TERT"
-OUTPATH="/home/louisshe/shortVariantVCF/out/masked_genome/"
-
 # Prerequisites:
 # - BEDTools
 # - seqkit
+# - seqtk
+# - GATK
+# - bwa
 
 source $(dirname $(realpath -s $0))/miscellaneous.sh
 source $(dirname $(realpath -s $0))/errorHandling.sh
 set -o errtrace
 trap 'catch_exit_status $? $LINENO $0' ERR
 
-[[ -f "${REF_GENOME}" ]] || exit 3;
 REF_GENOME_NAME=$(basename ${REF_GENOME} .fasta)
+
 if [[ -f "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta" ]]
 then
-    # TODOs: Check file validity
+    validate_mgenome "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta" "${REGION_BED}"
     echo -e >&2 "$(timestamp) WARNING: Masked genome already exists for ${PRIORITY_COMPONENT}"
     exit 0
 fi
 
-# TODOs:
 # - complementary BED
 bedtools subtract -a ${TOTAL_BED} -b ${REGION_BED} > "${OUTPATH}/$(basename ${TOTAL_BED} .bed)_exclude_${PRIORITY_COMPONENT}.bed.tmp"
 
 # - mask genome (main)
 awk '{print $1;}' "${REGION_BED}" | sort -uV | uniq > "${OUTPATH}/$(basename ${REGION_BED} .bed).bed.tmp"
-seqkit subseq -l 60 "${REF_GENOME}" "${OUTPATH}/$(basename ${REGION_BED} .bed).bed.tmp" > "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta.tmp"
-seqkit seq -l 60 -M "${OUTPATH}/$(basename ${TOTAL_BED} .bed)_exclude_${PRIORITY_COMPONENT}.bed.tmp" -n N "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta.tmp" > "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta"
+seqtk subseq -l 60 "${REF_GENOME}" "${OUTPATH}/$(basename ${REGION_BED} .bed).bed.tmp" > "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta.tmp"
+seqtk seq -l 60 -M "${OUTPATH}/$(basename ${TOTAL_BED} .bed)_exclude_${PRIORITY_COMPONENT}.bed.tmp" -n N "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta.tmp" > "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta"
 
 rm -f ${OUTPATH}/$(basename ${TOTAL_BED} .bed)_exclude_${PRIORITY_COMPONENT}.bed.tmp ${OUTPATH}/$(basename ${REGION_BED} .bed).bed.tmp ${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta.tmp
-
-# - check masked genome
-# - build index for genome
-# - validate masked genome
+index_genome "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta" bwtsw dict fai
+validate_mgenome "${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta" "${REGION_BED}"
+echo -e "$(timestamp) INFO: Masked genome created successfully. ("${OUTPATH}/${REF_GENOME_NAME}_${PRIORITY_COMPONENT}_masked.fasta")"
--- src/miscellaneous.sh
+++ src/miscellaneous.sh
@@ -9,5 +9,93 @@ function timestamp () {
     echo -e "[$(date +%a) $(date +%b-'%-m') $(date +'%-I':%M:%S%P)]"
 }
 
+function index_genome () {
+    # Possible arguments: bwtsw, fai, dict
+    set -e
+    local fasta="$1"
+    shift
+    while [[ $# -gt 0 ]]
+    do
+        local idx="$1"
+        shift
+
+        if [[ "${idx}" == "bwtsw" ]]
+        then
+            if  [[ ! -f ${fasta}.pac ]] || [[ ${fasta} -nt ${fasta}.pac ]] || \
+                [[ ! -f ${fasta}.bwt ]] || [[ ${fasta} -nt ${fasta}.bwt ]] || \
+                [[ ! -f ${fasta}.ann ]] || [[ ${fasta} -nt ${fasta}.ann ]] || \
+                [[ ! -f ${fasta}.amb ]] || [[ ${fasta} -nt ${fasta}.amb ]] || \
+                [[ ! -f ${fasta}.sa ]] || [[ ${fasta} -nt ${fasta}.sa ]]; then
+                bwa index -a bwtsw -b 380000000 "${fasta}"
+            fi
+        fi
+
+        if [[ "${idx}" == "fai" ]]
+        then
+            if [[ ! -f "${fasta}.fai" ]] || [[ "${fasta}" -nt "${fasta}.fai" ]]
+            then
+                samtools faidx "${fasta}"
+            fi
+        fi
+
+        if [[ "${idx}" == "dict" ]]
+        then
+            gatk CreateSequenceDictionary -R "${fasta}"
+        fi
+
+        echo -e "$(timestamp) INFO: Successfully created index file for ${fasta}"
+    done
+    return 0
+}
+
+function validate_mgenome {
+    local mgenome="$1"
+    local target_region="$2"
+
+    [[ -f "${mgenome}" ]] || return 3;
+    [[ -f "${target_region}" ]] || return 3;
+
+    # main checking
+    N_RECORDS=$(bedtools getfasta -fi "${mgenome}" -bed "${target_region}" | seqkit locate -p N -V 0 -G -I | { grep -c N$ || true; })
+    if [[ "${N_RECORDS}" -ne 0 ]]
+    then
+        echo -e >&2 "$(timestamp) ERROR: Masked genome ${mgenome} is invalid. Pls delete it and rerun."
+        echo -e >&2 "$(timestamp) ERROR: ${N_RECORDS} N bases detected. (Expected = 0)"
+        return -1
+    else
+        return 0
+    fi
+}
+
+function getDepth () {
+    local bamf=$1
+    local target_region=$2
+    local threads=$3
+    local readType=$4 # HQ
+
+    if [[ -f $bamf ]] && [[ -f $target_region ]] && [[ $(cat $target_region | wc -l) -gt 0 ]]
+    then
+        if [[ $readtype == "HQ" ]]
+        then
+            samtools view -h -@ $threads $bamf | grep -v XA:Z | samtools depth -a -b $target_region -Q 30 -g DUP,UNMAP,QCFAIL,SECONDARY - | awk '{sum+=$3} END{ printf "%s",sum/NR;}'
+        else
+            samtools depth -a -b $target_region -g DUP,UNMAP,QCFAIL,SECONDARY $bamf | awk '{sum+=$3} END{ printf "%s",sum/NR;}'
+        fi
+    else
+        return 127
+    fi
+}
+
+function isValidVCF () {
+    while [[ $# -gt 0 ]]
+    do
+        local vcf="$1"
+        shift
+
+        if [[ ! -f $vcf ]]; then return 1; fi
+
+        gatk ValidateVariants -V $vcf --validation-type-to-exclude ALL 1>/dev/null 2>&1
+    done
+}
 # Alias
 alias time="/usr/bin/time -f '$(timestamp) INFO: Elapsed time: %es\tMemory usage: %M KB\tCPU usage: %P'"
