a65f08e72712e1ca378adacac4914fe9af1d9e40
snakesch <shechunhing@yahoo.com.hk>
2024-10-08 13:09:15 +0800

    refactoring

--- prepare_recall_regions.py
+++ prepare_recall_regions.py
@@ -2,7 +2,6 @@ import argparse
 import cProfile
 import logging
 import os
-# import re
 from multiprocessing import Pool
 import sys
 
@@ -11,15 +10,14 @@ import pybedtools as pb
 import networkx as nx
 import graph_tool.all
 
-from graph_traversal import traverse_network_to_get_homology_counterparts
 from build_regions_from_PC_clusters import establish_beds_per_PC_cluster
 from seq import get_bam_frag_size
 from extract_SD_pairs_from_bam_json import extract_sd_coordinates_from_json
 from pick_multialign_regions import pick_region_by_depth
 from genome import Genome
 from homoseq_region import HOMOSEQ_REGION
-from graph import read_graphml, create_multiplex_graph
-from graph_query import extract_FC_NFC_pairs_from_graph, query_connected_nodes
+from graph_build import read_graphml, create_multiplex_graph
+from graph_query import extract_SD_paralog_pairs_from_graph, query_connected_nodes
 from convert_nodes_into_hierachical_beds import convert_nodes_into_hierachical_beds
 from sd_pairs import filter_umbrella_pairs
 
@@ -34,19 +32,16 @@ def deploy_PCs_for_SDrecall_main(ref_genome: str,
                                  input_bam: str,
                                  reference_sd_map: str,
                                  target_bed = "",
-                                 fraction_cutoff = 0.7,
+                                #  fraction_cutoff = 0.7,
                                  err_rate = 0.05, 
                                  threads = 10,
-                                 aggregation_resolution = .5,
+                                #  aggregation_resolution = .5,
                                  mq_cutoff = 20,
                                  target_tag = "target",
                                  profile_file = None):
 
     def imap_establish(tup_args):
         return establish_beds_per_PC_cluster(*tup_args)
-
-    def imap_traverse(tup_args):
-        return traverse_network_to_get_homology_counterparts(*tup_args)
 
     # if profile_file:
     #     pr = cProfile.Profile()
@@ -59,8 +54,8 @@ def deploy_PCs_for_SDrecall_main(ref_genome: str,
 
     # Step 0: Calculate the distribution of fragment sizes
     # avg_frag_size, std_frag_size = get_bam_frag_size(input_bam)
-    avg_frag_size, std_frag_size = 571.1, 120
-    logger.info(f"BAM {input_bam} has an average fragment size of {avg_frag_size}")
+    avg_frag_size, std_frag_size = 570.4, 150.7
+    logger.info(f"BAM {input_bam} has an average fragment size of {avg_frag_size}bp (std: {std_frag_size}bp)")
     
     ## Define names of intermediate files. Intermediate / final outputs will be written to work_dir.
     basename = os.path.basename(input_bam)
@@ -80,10 +75,10 @@ def deploy_PCs_for_SDrecall_main(ref_genome: str,
                                         target_tag="FCRs",
                                         genome_file=genome_file)
 
-    multi_align_bed_obj = pb.BedTool(multi_align_bed)
+    multi_align_bed_obj = pb.BedTool(multi_align_bed).sort()
     logger.info("The multialign bed extracted from {} is {} and it covers {} bp.".format(input_bam,
                                                                                       multi_align_bed,
-                                                                                      multi_align_bed_obj.sort().total_coverage()))
+                                                                                      multi_align_bed_obj.total_coverage()))
     
     # Step 2: Extract from the BAM file multialigned regions identified in step 1.
     ## Then extract the XA read_pairs with low MAPQ and output the results as JSON by sambamba
@@ -178,75 +173,67 @@ def deploy_PCs_for_SDrecall_main(ref_genome: str,
     expanded_total_bin_sd_df = total_bin_sd_df.copy()
     logger.debug("Total SD map contains {} regions:\n{}".format(total_bin_sd_df.shape[0], total_bin_sd_df.head(5).to_string(index=False)))
     
-    # Remove the duplicated SD combinations (i.e. same SD pair in different order)
+    ## Remove the duplicated SD combinations (i.e. same SD pair in different order)
     total_bin_sd_df.loc[:, "frozenset_indx"] = total_bin_sd_df.apply(lambda row: frozenset({ row["chr_1"]+":"+str(row["start_1"])+"-"+str(row["end_1"])+":"+row["strand1"], row["chr_2"]+":"+str(row["start_2"])+"-"+str(row["end_2"])+":"+row["strand2"]}), axis=1)
     total_bin_sd_df = total_bin_sd_df.drop_duplicates(subset="frozenset_indx").drop(columns=["frozenset_indx"])
     total_bin_sd_df.to_csv(os.path.join(work_dir, "filtered_SD_binary_map.tsv"), sep="\t", index=False)
     logger.info(f"After intersecting with refined SD reference, the total targeted SD region size for sample {input_bam} is {total_bin_sd_bed.sort().total_coverage()}bp.")
     logger.debug(f"Final SD map has shape {total_bin_sd_df.shape} and looks like: \n{total_bin_sd_df.head(5).to_string(index=False)}")
     
-    # Step 5: Create a graph on the total_bin_sd_df
+    # Step 5: Create the SD + PO graph
     graph_path = os.path.join(work_dir, basename + "-multiplexed_homologous_sequences.graphml")
     if os.path.exists(graph_path) and is_file_up_to_date(graph_path, [input_bam, target_bed, bam_json]):
         graph = read_graphml(graph_path)
     else:
-        graph = create_multiplex_graph( total_bin_sd_df, graph_path, 
-                                        overlap_frac_min = fraction_cutoff, 
-                                        avg_frag_size = avg_frag_size, 
-                                        std_frag_size = std_frag_size, 
-                                        resolution = aggregation_resolution,
-                                        threads = threads, 
-                                        target_bed = target_bed)
-    
-    # Step 6: Pick out the nodes that overlaps with target region, these intervals come from filtered reference SD map. Intervals might tangle(overlap) with each other
-    graph_bed = pb.BedTool.from_dataframe(expanded_total_bin_sd_df.drop_duplicates())
-    # Filter out the graph bed where the interval length should be larger than 150 bp (common read length)
+        graph = create_multiplex_graph( total_bin_sd_df, graph_path, threads = threads, target_bed = target_bed)
+
+    # Step 6: Extract SD + PO BED regions with depth issues caused by mapping ambiguity. Mutual overlap exists between intervals.
+    ## expanded_total_bin_sd_df is constructed from reference SD map, multi_align_bed_obj is constructed from specified target regions.
+    ## Filter out regions with length > 140bp (compared to 150bp reads)
+    graph_bed = pb.BedTool.from_dataframe(expanded_total_bin_sd_df)
     graph_bed = filter_bed_by_interval_size(graph_bed, 140)
     
-    # This is to identify which graph nodes overlap with the target regions to pool the reads to
-    intersect_df = graph_bed.intersect(multi_align_bed_obj.sort().merge(), wo=True).to_dataframe(disable_auto_names=True, names=["chrA", "startA", "endA", "strandA", 
-                                                                                                                                 "chrB", "startB", "endB", "strandB",
-                                                                                                                                 "mismatch_rate",
-                                                                                                                                 "chr_query", "start_query", "end_query", "overlap_len"])
+    ## Extract SD + PO BED regions with depth issues caused by mapping ambiguity
+    intersect_df = graph_bed.intersect(multi_align_bed_obj.merge(), wo=True).to_dataframe(disable_auto_names=True, names=["chrA", "startA", "endA", "strandA", 
+                                                                                                                            "chrB", "startB", "endB", "strandB",
+                                                                                                                            "mismatch_rate",
+                                                                                                                            "chr_target", "start_target", "end_target", "overlap_len"])
+    ## Sanity check
     intersect_df = intersect_df.loc[(intersect_df["endA"] > intersect_df["startA"]) & \
-                                    (intersect_df["end_query"] > intersect_df["start_query"]) & \
+                                    (intersect_df["end_target"] > intersect_df["start_target"]) & \
                                     (intersect_df["endB"] > intersect_df["startB"]), :]
 
-    by_query_interval = intersect_df.groupby(["chr_query", "start_query", "end_query"], as_index=False)
-    query_groups = [group for _, group in by_query_interval]
+    query_groups = [ g for _, g in intersect_df.groupby(["chr_target", "start_target", "end_target"], as_index=False) ]
     
-    # We just need to select the smallest FC_enwrapped HS intervals 
-    # Update: This filtering might filter out SD regions that map to other genomic regions so temporarily decaprecate it and observe the computation performance downstream
-    logger.info(f"Before the filtering, there are {intersect_df.shape[0]} intervals overlaps with the target regions: \n{intersect_df[:20].to_string(index=False)}\n")
+    logger.debug(f"Before filtering, {intersect_df.shape[0]} intervals overlaps with the target regions: \n{intersect_df[:20].to_string(index=False)}\n")
     with Pool(threads) as pool:
         results = pool.imap_unordered(filter_umbrella_pairs, query_groups)
         intersect_df = pd.concat(results, axis=0, ignore_index=True).drop_duplicates()
     
-    logger.info("Only {} necessary intervals overlaps with the target region intervals: \n{}\n".format(intersect_df.shape[0], intersect_df[:20].to_string(index=False)))
+    logger.debug("Only {} non-umbrella intervals overlaps target region: \n{}\n".format(intersect_df.shape[0], intersect_df[:20].to_string(index=False)))
     
     query_nodes = intersect_df.loc[:, ["chrA", "startA", "endA", "strandA"]].drop_duplicates().rename(columns={"chrA":"chr", "startA":"start", "endA":"end", "strandA":"strand"})
-    final_query_bed = pb.BedTool.from_dataframe(query_nodes)
-    final_query_bed.intersect(pb.BedTool(multi_align_bed), wo=True).saveas(os.path.join(work_dir, "coding_query_nodes.bed"))
-    logger.info("Save the coding overlap query nodes to this file: {}\n".format(os.path.join(work_dir, "coding_query_nodes.bed")))
-    
-    logger.info("Finally after picking up the merged the SD intervals, there are {} merged SDs from graph overlapped with exon, and it covers a region of {}bp.".format(query_nodes.shape[0],
-                                                                                                                                                                        final_query_bed.sort().total_coverage()))
-    
-    # Step 7. Now use a function to generate FC and NFC pairs for all the query nodes
+    if logger.level == logging.DEBUG:
+        final_query_bed = pb.BedTool.from_dataframe(query_nodes)
+        final_query_bed.intersect(multi_align_bed_obj, wo=True).saveas(os.path.join(work_dir, basename).replace(".bam", "target_overlapping_query_SD.bed")) # renamed from coding_query_nodes.bed
+        logger.debug("Target-overlapping SD regions saved to: {}".format(os.path.join(work_dir, basename).replace(".bam", "target_overlapping_query_SD.bed")))
+    logger.info("Identified {} distinct target-overlapping SDs from graph".format(query_nodes.shape[0]))
+
+    # Step 7. Pool all SD nodes and the corresponding paralogous regions from the SD + PO graph
     final_graph_path = graph_path.replace(".graphml", ".trim.annoPC.graphml")
-    fc_nfc_pairs, connected_qnode_components = extract_FC_NFC_pairs_from_graph( query_nodes, 
+    sd_paralog_pairs, connected_qnode_components = extract_SD_paralog_pairs_from_graph( query_nodes, 
                                                                                 graph, 
                                                                                 graph_path = final_graph_path, 
                                                                                 avg_frag_size = avg_frag_size, 
                                                                                 std_frag_size = std_frag_size, 
                                                                                 threads=threads )
-    logger.info("After extracting the FC-NFC pairs, we got {} FC-NFC pairs".format(len(fc_nfc_pairs)))
+    logger.info("{} SD-paralog pairs pooled from SD + PO graph".format(len(sd_paralog_pairs)))
     
     # Now we need to test which FC-NFC pairs can be merged together (meaning FCs cannot appear in NFCs covered region)
     # Step 8: Pass the query nodes to function and get the cluster of SD intervals (The query nodes here is a pandas dataframe object)
     pair_graph_path = graph_path.replace(".graphml", ".pair.graphml")
-    logger.info("Now we about to use the extract FC-NFC pairs to find out which of them can be put in the same masked genome")
-    all_results, fc_nfc_dict = query_connected_nodes(fc_nfc_pairs, 
+    logger.info("Now we about to use the extract SD-paralog pairs to find out which of them can be put in the same masked genome")
+    all_results, fc_nfc_dict = query_connected_nodes(sd_paralog_pairs, 
                                                      connected_qnode_components, 
                                                      multiplex_graph = graph,
                                                      threads = threads,
@@ -291,10 +278,8 @@ def main():
     parser.add_argument('-i', '--input_bam', required=True, help='Input BAM file.')
     parser.add_argument('-m', '--reference_sd_map', required=True, help='Reference structural variant map file.')
     parser.add_argument('-b', '--target_bed', default="", help='Optional target BED file.')
-    parser.add_argument('--fraction_cutoff', type=float, default=0.7, help='Fraction cutoff for analysis.')
     parser.add_argument('-e', '--error_rate', type=float, default=0.05, help='Error rate for determining reads with extreme template lengths.')
     parser.add_argument('-t', '--threads', type=int, default=10, help='Number of threads to use.')
-    parser.add_argument('--aggregation_resolution', type=float, default=0.5, help='Resolution for aggregation.')
     parser.add_argument('--mq_cutoff', type=int, default=20, help='Mapping quality cutoff.')
     parser.add_argument('--target_tag', type=str, default="target", help='Optional target tag for filtering.')
     parser.add_argument('-p', '--profile_file', type=str, default=None, help='Optional profile file for tuning.')
@@ -316,10 +301,8 @@ def main():
         input_bam=args.input_bam,
         reference_sd_map=args.reference_sd_map,
         target_bed=args.target_bed,
-        fraction_cutoff=args.fraction_cutoff,
         err_rate=args.error_rate,
         threads=args.threads,
-        aggregation_resolution=args.aggregation_resolution,
         mq_cutoff=args.mq_cutoff,
         target_tag=args.target_tag,
         profile_file=args.profile_file
